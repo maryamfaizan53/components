6be5f251c8c5bff2c6cd282f563b8d9528a039af
Maryam faizan <118754207+maryamfaizan53@users.noreply.github.com>
2024-09-24 19:44:18 +0500

    done

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/header";
+import About from "./components/about";
+import Contact from "./components/contact";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
